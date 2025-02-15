08bb6da20944e6a59ef337c7f61fdb9e2b804a11
JoeTheGoat <81477144+frc6324-team-laptop@users.noreply.github.com>
2025-02-15 15:45:44 -0500

    Create main page
    Created header, main page, and footer, as well as installed icons for links.

---
+++ team6324_website/src/components/Header.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<header class="heading">
+    <div class="title">
+        <h1>Salem Robotics</h1>
+    </div>
+    <div class="links">
+        <h3 class="page_link">Community</h3>
+        <h3 class="page_link">News</h3>
+        <h3 class="page_link">History</h3>
+        <h3 class="page_link">Sponsors</h3>
+        <h3 class="page_link" style="padding-right:30px">Contact Us</h3>
+    </div>
+</header>
+</template>
+
+<style scoped>
+.heading {
+    display: flex;
+    width: 100%;
+    background-color: rgb(121 138 155);
+    border-radius: 20px;
+    margin-top: 10px;
+    margin-bottom: 15px;
+    color: white;
+}
+.title {
+    padding-left: 30px;
+    width: 30%;
+}
+.links {
+    width: 70%;
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: right;
+}
+.page_link {
+    font-weight: bold;
+    padding-left: 30px;
+}
+</style>
