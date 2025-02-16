202a2170fa6fa2dcfd2f95239a55aa6a733e3428
Cooper Bouchard <cobouchard11@gmail.com>
2025-02-15 21:35:42 -0500

    Separate header & footer from App component, added page system
    Added page system in page.ts, renamed photos, deleted unnecessary auto-generated files, declare components in main.ts

---
+++ team6324_website/src/components/PageHeader.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { setPage } from './util/page'
+import MainPage from './pages/MainPage.vue'
+import CommunityPage from './pages/CommunityPage.vue'
+</script>
+
+<template>
+  <header class="heading">
+    <h1>
+      <a class="title" href="#" @click.prevent="setPage(MainPage)">Salem Robotics</a>
+    </h1>
+    <div class="links">
+      <!-- href="#" redirects to the top of the current page, and @click.prevent="..." changes the tab -->
+      <a href="#" @click.prevent="setPage(CommunityPage)"><h3 class="page_link">Community</h3></a>
+      <!-- TODO: add more links -->
+      <h3 class="page_link">News</h3>
+      <h3 class="page_link">History</h3>
+      <h3 class="page_link">Sponsors</h3>
+      <h3 class="page_link" style="padding-right: 30px">Contact Us</h3>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.heading {
+  display: flex;
+  width: 98%;
+  height: 100px;
+  align-items: center;
+  background-color: rgb(121 138 155);
+  border-radius: 20px;
+  margin-top: 10px;
+  margin-bottom: 15px;
+  color: white;
+
+  h1 .title {
+    padding: 0;
+    padding-left: 30px;
+    border: none;
+    background: none;
+    width: 30%;
+    color: white;
+    font-weight: bold;
+  }
+  .links {
+    width: 70%;
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: right;
+    .page_link {
+      font-weight: bold;
+      padding-left: 30px;
+      color: white;
+    }
+  }
+}
+</style>
---
+++ team6324_website/src/components/pages/CommunityPage.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { setPage } from '../util/page'
+import MainPage from './MainPage.vue'
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="warning">Woah there!</h1>
+
+    <br />
+
+    <h1 class="warning">This page is still under construction!</h1>
+
+    <br />
+    <br />
+    <br />
+
+    <a class="ret_link" href="#" @click.prevent="setPage(MainPage)"
+      ><u>Go back to the home page</u></a
+    >
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: inline-block;
+  align-items: center;
+  justify-items: center;
+  justify-content: center;
+  width: 100%;
+  padding-bottom: 150px;
+  .warning {
+    font-weight: bold;
+    font-size: 30px;
+    color: yellow;
+  }
+  .ret_link {
+    font-size: 20px;
+    color: white;
+  }
+}
+</style>
